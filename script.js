67e8e77cd42f58898be2804965f8715cd23ffc3a
George Essilfie <Georgeessilfie91@hotmail.com>
2018-07-12 02:34:11 -0400

    modal+slider beginning

--- script.js
+++ script.js
@@ -48,3 +48,82 @@ function outsideClick(e){
     modal.style.display = 'none';
     }
 }
+
+//For Modal #2
+
+//Get the modal element
+var modal2 = document.getElementById('simpleModal2');
+//Get open modal button
+var modalBtn2 = document.getElementById('modalBtn2');
+//Get close button
+var closeBtn2 = document.getElementsByClassName('closeBtn2')[0];
+
+//Listen for open click
+modalBtn2.addEventListener('click', openModal2);
+
+//Listen for close click
+closeBtn2.addEventListener('click', closeModal2);
+
+//Listen for outside click
+window.addEventListener('click', outsideClick2);
+
+//Function to open modal
+function openModal2(){
+    modal2.style.display = 'block';
+}
+
+//Function to close modal
+function closeModal2(){
+    modal2.style.display = 'none';
+}
+
+//Function to close modal if outside click
+function outsideClick2(e){
+    if(e.target == modal2){
+    modal2.style.display = 'none';
+    }
+}
+
+//For Modal3
+//Get the modal element
+var modal3 = document.getElementById('simpleModal3');
+//Get open modal button
+var modalBtn3 = document.getElementById('modalBtn3');
+//Get close button
+var closeBtn3 = document.getElementsByClassName('closeBtn3')[0];
+
+//Listen for open click
+modalBtn3.addEventListener('click', openModal3);
+
+//Listen for close click
+closeBtn3.addEventListener('click', closeModal3);
+
+//Listen for outside click
+window.addEventListener('click', outsideClick3);
+
+//Function to open modal
+function openModal3(){
+    modal3.style.display = 'block';
+}
+
+//Function to close modal
+function closeModal3(){
+    modal3.style.display = 'none';
+}
+
+//Function to close modal if outside click
+function outsideClick3(e){
+    if(e.target == modal3){
+    modal2.style.display = 'none';
+    }
+}
+
+
+
+
+
+
+
+
+
+
